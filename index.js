877dcb38f831d78c06bc78edf7f4f85713274ee1
duyisking <duyisking1060@gmail.com>
2017-10-24 11:22:55 +0700

    Connect to psql successfully!

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var pg = require('pg');
 
 var app = express();
 var client = new pg.Client({
-  connectionString: 'postgres://swkcgdxapapojz:e2f595b6946c021e1f2ce02cd3852cdce6a5e391aa358e5ebc204dc3aa158e54@ec2-23-23-249-169.compute-1.amazonaws.com:5432/d34l4ng8fd8g0a',
-})
+  user: "swkcgdxapapojz",
+  password: "e2f595b6946c021e1f2ce02cd3852cdce6a5e391aa358e5ebc204dc3aa158e54",
+  database: "d34l4ng8fd8g0a",
+  port: 5432,
+  host: "ec2-23-23-249-169.compute-1.amazonaws.com",
+  ssl: true
+}); 
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -22,11 +27,10 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-console.log(process.env.DATABASE_URL);
-
 app.get('/db', function (req, res) {
-  client.query('SELECT * from test_table', (err, res) => {
-    res.send(res);
+  client.connect();
+  client.query('SELECT * from test_table', (err, re) => {
+    res.send(re);
     client.end();
   })
 
