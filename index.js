fe4be5a48d195f0d05b2261f47d413c521761cca
duyisking <duyisking1060@gmail.com>
2017-10-25 18:00:36 +0700

    Change CORS

--- index.js
+++ index.js
@@ -34,7 +34,10 @@ app.set('port', (process.env.PORT || port));
 app.use(express.static(__dirname + '/public'));
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import axios from 'axios'
 
 import Devices from 'components/devices.jsx'
 
 import './sass/main.sass'
 
 import 'images/apps.png'
+
+axios.post('https://reactiot.herokuapp.com/insert', {
+    data: {
+        id: '101',
+        name: 'ABC',
+        latitude: 'null',
+        longitude: 'null',
+        status: 'enable'
+    }
+})
+    .then(function (response) {
+        console.log(response)
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 
 ReactDom.render(
     <div id="content">
